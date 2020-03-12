60bca990486d19910ba9b1eb5f659d818c8bd5a7
Phil Schanely <philschanely@gmail.com>
2020-03-11 22:23:30 -0400

    Initial files

---
+++ README.md
@@ -0,0 +1,12 @@
+# LifeAsWeLiveIt.com WordPress Theme
+
+WordPress Theme for LifeAsWeLiveIt.com.
+
+[http://lifeasweliveit.com](http://schanelyphotography.com)
+
+Run locally:
+
+```
+npm install
+npm start
+```
---
+++ src/_theme/functions.php
@@ -0,0 +1,17 @@
+<?php
+$lawli_com_theme_parent_style = 'parent-style';
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles_and_scripts' );
+function my_theme_enqueue_styles_and_scripts() {
+
+  // Enqueue the styles
+  wp_enqueue_style( $lawli_com_theme_parent_style$lawli_com_theme_parent_style, get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array( $lawli_com_theme_parent_style ),
+    wp_get_theme()->get('Version')
+  );
+
+  // Enqueue the styles
+  wp_enqueue_script( 'app', get_template_directory_uri() . '/app.js', array(), 1.0, true);
+}
