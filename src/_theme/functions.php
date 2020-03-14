2bc22edc33897bdb39acf8ba15ac0a7d15d1a240
Phil Schanely <philschanely@gmail.com>
2020-03-14 13:03:22 -0400

    Changes for live launch

--- src/_theme/functions.php
+++ src/_theme/functions.php
@@ -5,7 +5,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles_and_scripts' );
 function my_theme_enqueue_styles_and_scripts() {
 
   // Enqueue the styles
-  wp_enqueue_style( $lawli_com_theme_parent_style$lawli_com_theme_parent_style, get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( $lawli_com_theme_parent_style, get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'child-style',
     get_stylesheet_directory_uri() . '/style.css',
     array( $lawli_com_theme_parent_style ),
